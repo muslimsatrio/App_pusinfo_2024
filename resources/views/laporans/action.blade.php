0e9700a9e4daf23021e5c9017fb423aa2416ca9c
muslimsatrio <muslimsatrio93@gmail.com>
2024-01-04 07:05:39 +0700

    Update action.blade.php
    update button edit, view dan delete

--- resources/views/laporans/action.blade.php
+++ resources/views/laporans/action.blade.php
@@ -1,19 +1,18 @@
 <div class="container">
     <div class="row">
         <div class="col align-self-center">
-            <a href="{{ route('laporans.show',$id) }}" data-toggle="tooltip" data-original-title="Show" class="  btn btn-primary ">
-                View <i class="fa fa-eye" aria-hidden="true"></i>
+            <a href="{{ route('laporans.show',$id) }}" data-toggle="tooltip" data-original-title="Show" class="btn btn-primary ">
+                 <i class="fa fa-eye" aria-hidden="true"></i>
                 </a>
         </div>
         <div class="col align-self-center">
             <a href="{{ route('laporans.edit',$id) }}" data-toggle="tooltip" data-original-title="Edit" class="edit btn btn-success edit">
-                Edit <i class="fa fa-pencil" aria-hidden="true"></i>
+             <i class="fa fa-pencil" aria-hidden="true"></i>
                 </a>
         </div>
-
         <div class="col align-self-center">
                 <a href="javascript:void(0)" data-id="{{ $id }}" data-toggle="tooltip" data-original-title="Delete" class="delete btn btn-danger">
-                    Delete <i class="fa fa-trash" aria-hidden="true"></i>
+                     <i class="fa fa-trash" aria-hidden="true"></i>
                     </a>
         </div>
 
