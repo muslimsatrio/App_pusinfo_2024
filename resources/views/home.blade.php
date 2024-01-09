1f277c2c870049002cc676740cb8e3094a19920d
TexTrax <155422938+TexTrax@users.noreply.github.com>
2024-01-09 08:47:11 +0700

    Login edit
    Login edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="plugins/asfie/style.css" rel="stylesheet" >
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
   
 </head>
-<body style="background-image: url('public/image/bg.eps')">
+<!-- <body style="background-image: url('public/image/bg.eps')">
 
 <div class="container-fluid p-5 bg-primary text-white text-center">
   <h1>Database Management System</h1>
@@ -70,7 +71,45 @@
   </div>
 </div>
 
-</body>
+</body>  -->
+
+<body>
+        <div class="container-fluid">
+        <!-- <div class="col-md-12"> -->
+        @if(session('success'))
+            <p class="alert alert-success">{{ session('success') }}</p>
+            @endif
+            @if($errors->any())
+            @foreach($errors->all() as $err)
+            <p class="alert alert-danger">{{ $err }}</p>
+            @endforeach
+            @endif
+            <form action="{{ route('login.action') }}" method="POST" class="mx-auto">
+                @csrf
+                <h4 class="text-center">Login</h4>
+                <div class="mb-3 mt-5">
+                  <label for="exampleInputEmail1" class="form-label">User Name</label>
+                  <input class="form-control" type="username" name="username" value="{{ old('username') }}" />
+                  <!-- <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"> -->
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">Password</label>
+                  <input class="form-control" type="password" name="password" />
+                  <!-- <input type="password" class="form-control" id="exampleInputPassword1"> -->
+                  <div id="emailHelp" class="form-text mt-3">Forget password ?</div>
+                </div>
+              
+                <button type="submit" class="btn btn-primary mt-5">Login</button>
+              </form>
+        </div>
+        <!-- </div> -->
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+
 </html>
 
 {{-- batas ori --}}
